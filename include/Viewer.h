a0f9f3d09bef66d60824c1d205126e7ca4b05df3
Michael Shomin <michael@shom.in>
2016-04-12 02:31:54 -0400

    seperate Viewer and Pangoli

--- include/Viewer.h
+++ include/Viewer.h
@@ -22,8 +22,6 @@
 #ifndef VIEWER_H
 #define VIEWER_H
 
-#include "FrameDrawer.h"
-#include "MapDrawer.h"
 #include "Tracking.h"
 #include "System.h"
 
@@ -33,48 +31,41 @@ namespace ORB_SLAM2
 {
 
 class Tracking;
-class FrameDrawer;
-class MapDrawer;
 class System;
 
-class Viewer
-{
-public:
-    Viewer(System* pSystem, FrameDrawer* pFrameDrawer, MapDrawer* pMapDrawer, Tracking *pTracking, const string &strSettingPath);
+class Viewer {
+ public:
+  Viewer();
 
-    // Main thread function. Draw points, keyframes, the current camera pose and the last processed
-    // frame. Drawing is refreshed according to the camera fps. We use Pangolin.
-    void Run();
+  virtual void Run();
+  
+  void RequestFinish();
+  
+  void RequestStop();
+  
+  bool isFinished();
+  
+  bool isStopped();
+  
+  void Release();
+  
+  virtual void UpdateFrame(Tracking *pTracker);
+  virtual void SetCurrentCameraPose(const cv::Mat &Tcw);
+  virtual void Register(System* pSystem);
+  virtual void Finalize(void);
 
-    void RequestFinish();
-
-    void RequestStop();
-
-    bool isFinished();
-
-    bool isStopped();
-
-    void Release();
-
-private:
-
-    bool Stop();
-
-    System* mpSystem;
-    FrameDrawer* mpFrameDrawer;
-    MapDrawer* mpMapDrawer;
-    Tracking* mpTracker;
-
-    // 1/fps in ms
-    double mT;
-    float mImageWidth, mImageHeight;
-
-    float mViewpointX, mViewpointY, mViewpointZ, mViewpointF;
-
-    bool CheckFinish();
-    void SetFinish();
-    bool mbFinishRequested;
+  protected:
+	System* mpSystem;
     bool mbFinished;
+    bool Stop();
+	bool CheckFinish();
+    void SetFinish();
+
+
+  private:
+
+
+    bool mbFinishRequested;
     std::mutex mMutexFinish;
 
     bool mbStopped;
